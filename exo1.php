c2c386ba12a7d3dea7fa29fb743cbb4595ad479a
julie-devos <julie.devos@epitech.eu>
2020-12-07 18:16:03 +0100

    initial commit

---
+++ exo1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Exercice 1-Partie 2</title>
+</head>
+<body>
+
+<?php
+$age=12;
+?>
+<?="$age"?><br>
+
+<?=$age<=17 ? "Vous êtes mineur." : NULL ?><br>
+
+<?php
+$age=18;
+?>
+<?=$age>=18 ? "Vous êtes majeur." : "Vous êtes toujours mineur.";?><br>
+
+</body>
+</html>
